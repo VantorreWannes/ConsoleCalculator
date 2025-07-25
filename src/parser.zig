d562ad0d71eda5b06848aa672cc028041295fdbf
Wannes Vantorre <vantorrewannes@gmail.com>
2025-07-25 18:43:53 +0200

    feature: Implement applyFunction helper function.

--- src/parser.zig
+++ src/parser.zig
@@ -46,19 +46,19 @@ fn parseExpression(self: *Parser) ParseError!Number {
 
 // term -> factor (('*' | '/') factor)*
 fn parseTerm(self: *Parser) ParseError!Number {
-    var factor = try self.parseFactor();
+    var factor = try self.parsePower();
     while (self.index < self.tokens.len) {
         const token = self.tokens[self.index];
         if (std.meta.activeTag(token) != TokenTag.operator) break;
         switch (token.operator) {
             .multiply => {
                 self.index += 1;
-                const other_factor = try self.parseFactor();
+                const other_factor = try self.parsePower();
                 factor = factor.mul(other_factor);
             },
             .divide => {
                 self.index += 1;
-                const other_factor = try self.parseFactor();
+                const other_factor = try self.parsePower();
                 factor = factor.div(other_factor);
             },
             else => break,
@@ -67,7 +67,13 @@ fn parseTerm(self: *Parser) ParseError!Number {
     return factor;
 }
 
-// factor -> NUMBER | '(' expression ')' | '-' factor
+// power -> factor ('^' power)?
+fn parsePower(self: *Parser) ParseError!Number {
+    _ = self;
+    return Number.init(1, 0);
+}
+
+// factor -> NUMBER | CONSTANT | FUNCTION '(' expression ')' | '(' expression ')' | '-' factor
 fn parseFactor(self: *Parser) ParseError!Number {
     if (self.index >= self.tokens.len) return ParseError.MissingTokens;
     switch (self.tokens[self.index]) {
@@ -75,179 +81,52 @@ fn parseFactor(self: *Parser) ParseError!Number {
             self.index += 1;
             return number;
         },
-        .parenthesis => |parenthesis| {
-            if (parenthesis != .open) return ParseError.InvalidToken;
-            self.index += 1;
-            const expression = try self.parseExpression();
-            if (self.index >= self.tokens.len) return ParseError.MissingTokens;
-            const token = self.tokens[self.index];
-            if (std.meta.activeTag(token) != TokenTag.parenthesis or token.parenthesis != .close) return ParseError.MissingTokens;
-            self.index += 1;
-            return expression;
-        },
-        .operator => |operator| {
-            if (operator != .minus) return ParseError.InvalidToken;
-            self.index += 1;
-            const factor = try self.parseFactor();
-            return factor.neg();
-        },
-        else => return ParseError.InvalidToken,
     }
 }
 
-test parse {
-    const tokens = [_]Token{
-        Token{ .number = Token.Number.init(2, 0) },
-        Token{ .operator = .plus },
-        Token{ .number = Token.Number.init(3, 0) },
-        Token{ .operator = .multiply },
-        Token{ .number = Token.Number.init(2, 0) },
+pub fn applyFunction(function: Token.Function, number: Number) Number {
+    return switch (function) {
+        .absolute => Number.init(std.math.complex.abs(number), 0),
+        .conjugate => std.math.complex.conj(number),
+        .exponential => std.math.complex.exp(number),
+        .gamma => blk: {
+            const re = std.math.gamma(f64, number.re);
+            const im = if (number.im == 0) 0 else std.math.gamma(f64, number.im);
+            break :blk Number.init(re, im);
+        },
+        .ceiling => blk: {
+            const re = std.math.ceil(number.re);
+            const im = std.math.ceil(number.im);
+            break :blk Number.init(re, im);
+        },
+        .floor => blk: {
+            const re = std.math.floor(number.re);
+            const im = std.math.floor(number.im);
+            break :blk Number.init(re, im);
+        },
+        .real => Number.init(number.re, 0),
+        .imaginary => Number.init(number.im, 0),
+        .squareRoot => std.math.complex.sqrt(number),
+        .logarithm10 => blk: {
+            const log10 = std.math.complex.log(Number.init(10, 0));
+            break :blk std.math.complex.log(number).div(log10);
+        },
+        .logarithm2 => blk: {
+            const log2 = std.math.complex.log(Number.init(2, 0));
+            break :blk std.math.complex.log(number).div(log2);
+        },
+        .logarithmE => std.math.complex.log(number),
+        .sine => std.math.complex.sin(number),
+        .cosine => std.math.complex.cos(number),
+        .tangent => std.math.complex.tan(number),
+        .hyperbolicSine => std.math.complex.sinh(number),
+        .hyperbolicCosine => std.math.complex.cosh(number),
+        .hyperbolicTangent => std.math.complex.tanh(number),
+        .inverseSine => std.math.complex.asin(number),
+        .inverseCosine => std.math.complex.acos(number),
+        .inverseTangent => std.math.complex.atan(number),
+        .inverseHyperbolicSine => std.math.complex.asinh(number),
+        .inverseHyperbolicCosine => std.math.complex.acosh(number),
+        .inverseHyperbolicTangent => std.math.complex.atanh(number),
     };
-    var parser = Parser.init(&tokens);
-    const result = try parser.parse();
-    const expected = Number.init(8, 0);
-    try std.testing.expectEqual(expected, result);
-}
-
-// expression -> term (('+' | '-') term)*
-test parseExpression {
-    // expression -> term
-    {
-        const tokens = [_]Token{Token{ .number = Token.Number.init(2, 0) }};
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseExpression();
-        const expected = Token.Number.init(2, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // expression -> term '+' term
-    {
-        const tokens = [_]Token{ Token{ .number = Token.Number.init(2, 0) }, Token{ .operator = .plus } };
-        var parser = Parser.init(&tokens);
-        const result = parser.parseExpression();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{
-            Token{ .number = Token.Number.init(2, 0) },
-            Token{ .operator = .plus },
-            Token{ .number = Token.Number.init(2, 0) },
-        };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseExpression();
-        const expected = Token.Number.init(4, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // expression -> term '-' term
-    {
-        const tokens = [_]Token{ Token{ .number = Token.Number.init(2, 0) }, Token{ .operator = .minus } };
-        var parser = Parser.init(&tokens);
-        const result = parser.parseExpression();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{
-            Token{ .number = Token.Number.init(2, 0) },
-            Token{ .operator = .minus },
-            Token{ .number = Token.Number.init(2, 0) },
-        };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseExpression();
-        const expected = Token.Number.init(0, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-}
-
-// term -> factor (('*' | '/') factor)*
-test parseTerm {
-    // term -> factor
-    {
-        const tokens = [_]Token{Token{ .number = Token.Number.init(2, 0) }};
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseTerm();
-        const expected = Token.Number.init(2, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // term -> factor '*' factor
-    {
-        const tokens = [_]Token{ Token{ .number = Token.Number.init(2, 0) }, Token{ .operator = .multiply } };
-        var parser = Parser.init(&tokens);
-        const result = parser.parseTerm();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{
-            Token{ .number = Token.Number.init(2, 0) },
-            Token{ .operator = .multiply },
-            Token{ .number = Token.Number.init(2, 0) },
-        };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseTerm();
-        const expected = Token.Number.init(4, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // term -> factor '/' factor
-    {
-        const tokens = [_]Token{ Token{ .number = Token.Number.init(2, 0) }, Token{ .operator = .divide } };
-        var parser = Parser.init(&tokens);
-        const result = parser.parseTerm();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{
-            Token{ .number = Token.Number.init(2, 0) },
-            Token{ .operator = .divide },
-            Token{ .number = Token.Number.init(2, 0) },
-        };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseTerm();
-        const expected = Token.Number.init(1, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-}
-
-// factor -> NUMBER | '(' expression ')' | '-' factor
-test parseFactor {
-
-    // factor -> NUMBER
-    {
-        const tokens = [_]Token{Token{ .number = Token.Number.init(1, 0) }};
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseFactor();
-        const expected = Token.Number.init(1, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // factor -> '(' expression ')'
-    {
-        const tokens = [_]Token{Token{ .parenthesis = .open }};
-        var parser = Parser.init(&tokens);
-        const result = parser.parseFactor();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{ Token{ .parenthesis = .open }, Token{ .number = Token.Number.init(1, 0) } };
-        var parser = Parser.init(&tokens);
-        const result = parser.parseFactor();
-        try std.testing.expectError(ParseError.MissingTokens, result);
-    }
-    {
-        const tokens = [_]Token{ Token{ .parenthesis = .open }, Token{ .number = Token.Number.init(1, 0) }, Token{ .parenthesis = .close } };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseFactor();
-        const expected = Token.Number.init(1, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-
-    // factor -> '-' factor
-    {
-        const tokens = [_]Token{ Token{ .operator = .minus }, Token{ .number = Token.Number.init(1, 0) } };
-        var parser = Parser.init(&tokens);
-        const result = try parser.parseFactor();
-        const expected = Token.Number.init(1, 0).neg();
-        try std.testing.expectEqual(expected, result);
-    }
 }
--- src/root.zig
+++ src/root.zig
@@ -15,34 +15,6 @@ pub fn calculate(allocator: std.mem.Allocator, input: []const u8) CalculateError
     return parser.parse();
 }
 
-test calculate {
-    const allocator = std.testing.allocator;
-    {
-        const input = "2 + 3 * 2";
-        const result = try calculate(allocator, input);
-        const expected = Number.init(8, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-    {
-        const input = "3*(7-2*(16)-5*-7)-3+2*4";
-        const result = try calculate(allocator, input);
-        const expected = Number.init(35, 0);
-        try std.testing.expectEqual(expected, result);
-    }
-    {
-        const input = "3*(2+3";
-        const result = calculate(allocator, input);
-        const expected = CalculateError.MissingTokens;
-        try std.testing.expectError(expected, result);
-    }
-    {
-        const input = "+1++1";
-        const result = calculate(allocator, input);
-        const expected = CalculateError.InvalidToken;
-        try std.testing.expectError(expected, result);
-    }
-}
-
 test {
     std.testing.refAllDecls(Tokenizer);
     std.testing.refAllDecls(Parser);
